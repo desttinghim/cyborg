6125887b61c43dbaaa758a5d2620d9d83c458f4d
Louis Pearson <opensource@louispearson.work>
2022-10-13 02:11:50 -0600

    Add xml data type definitions

--- src/binxml.zig
+++ src/binxml.zig
@@ -1,70 +1,283 @@
 const std = @import("std");
 
-const Event = enum(u4) {
-    DocumentStart = 0,
-    DocumentEnd = 1,
-    TagStart = 2,
-    TagEnd = 3,
-    Text = 4,
-    CDSect = 5,
-    EntityRef = 6,
-    IgnorableWhitespace = 7,
-    ProcessingInstruction = 8,
-    Comment = 9,
-    DocDecl = 10,
-    Attribute = 0xF,
+const Type = enum(u16) {
+    Null = 0x0000,
+    StringPool = 0x0001,
+    Table = 0x0002,
+    Xml = 0x0003,
+
+    XmlStartNamespace = 0x0100,
+    XmlEndNamespace = 0x0101,
+    XmlStartElement = 0x0102,
+    XmlEndElement = 0x0103,
+    XmlCData = 0x0104,
+
+    XmlResourceMap = 0x0180,
+
+    TablePackage = 0x0200,
+    TableType = 0x0201,
+    TableTypeSpec = 0x0202,
+    TableLibrary = 0x0203,
 };
 
-const Type = enum(u4) {
-    Null = 1,
-    String = 2,
-    StringInterned = 3,
-    BytesHex = 4,
-    BytesBase64 = 5,
-    Int = 6,
-    IntHex = 7,
-    Long = 8,
-    LongHex = 9,
-    Float = 10,
-    Double = 11,
-    BoolTrue = 12,
-    BoolFalse = 13,
-};
-
-const ByteLookup = struct {
-    pos: usize,
-    length: u16,
-};
-
-const TypeValue = union(Type) {
-    Null,
-    String: []const u8, // Length
-    StringInterned: u16, // Id
-    BytesHex: ByteLookup,
-    BytesBase64: ByteLookup, // Length
-    Int: u32,
-    IntHex: u32,
-    Long: u64,
-    LongHex: u64,
-    Float: f32,
-    Double: f64,
-    BoolTrue,
-    BoolFalse,
-};
-
-const Token = packed struct(u8) {
-    event: Event,
+const ResourceChunk = struct {
     type: Type,
+    header_size: u16,
+    size: u16,
 };
 
-const TokenValue = struct {
-    token: Token,
-    value: TypeValue,
+const DataType = enum(u8) {
+    Null = 0x00,
+    Reference = 0x01,
+    Attribute = 0x02,
+    String = 0x03,
+    Float = 0x04,
+    Dimension = 0x05,
+    Fraction = 0x06,
+    DynReference = 0x07,
+    DynAttribute = 0x08,
+    IntDec = 0x10,
+    IntHex = 0x11,
+    IntBool = 0x12,
+    IntColorARGB8 = 0x1c,
+    IntColorRGB8 = 0x1d,
+    IntColorARGB4 = 0x1e,
+    IntColorRGB4 = 0x1f,
 };
 
-const InternedString = struct {
-    pos: u16,
-    length: u16,
+const DimensionUnit = enum(u4) {
+    Pixels = 0x0,
+    DeviceIndependentPixels = 0x1,
+    ScaledDeviceIndependentPixels = 0x2,
+    Points = 0x3,
+    Inches = 0x4,
+    Millimeters = 0x5,
+    Fraction = 0x6,
+};
+
+const FractionUnit = packed struct(u8) {
+    unit: enum(u1) {
+        Basic,
+        Parent,
+    },
+    radix: enum(u3) {
+        r23p0 = 0,
+        r16p7 = 1,
+        r8p15 = 2,
+        r0p23 = 3,
+    },
+};
+
+const NullType = enum(u1) {
+    Undefined = 0,
+    Empty = 1,
+};
+
+const Value = struct {
+    size: u16,
+    res0: u8,
+    datatype: DataType,
+    data: u32,
+};
+
+const TableRef = struct {
+    ident: u32,
+};
+
+const StringPool = struct {
+    const Ref = struct {
+        index: u32,
+    };
+
+    const Header = struct {
+        header: ResourceChunk,
+        string_count: u32,
+        style_count: u32,
+        flags: packed struct(u32) {
+            sorted: bool,
+            utf8: bool,
+        },
+        strings_start: u32,
+        styles_start: u32,
+    };
+
+    const Span = struct {
+        name: Ref,
+        first_char: u32,
+        last_char: u32,
+    };
+};
+
+const XMLTree = struct {
+    const Header = struct {
+        header: ResourceChunk,
+    };
+
+    const Node = struct {
+        header: ResourceChunk,
+        line_number: u32,
+        comment: StringPool.Ref,
+    };
+
+    const CDataExtended = struct {
+        data: StringPool.Ref,
+        value: Value,
+    };
+
+    const NamespaceExtended = struct {
+        prefix: StringPool.Ref,
+        uri: StringPool.Ref,
+    };
+
+    const EndElementExtended = struct {
+        namespace: StringPool.Ref,
+        name: StringPool.Ref,
+    };
+
+    const AttributeExtended = struct {
+        namespace: StringPool.Ref,
+        name: StringPool.Ref,
+        start: u16,
+        size: u16,
+        count: u16,
+        id_index: u16,
+        class_index: u16,
+        style_index: u16,
+    };
+
+    const Attribute = struct {
+        namespace: StringPool.Ref,
+        name: StringPool.Ref,
+        raw_value: StringPool.Ref,
+        typed_value: Value,
+    };
+};
+
+const ResourceTable = struct {
+    const Header = struct {
+        header: ResourceChunk,
+        package_count: u32,
+    };
+
+    const Package = struct {
+        header: ResourceChunk,
+        id: u32,
+        name: [127:0]u16,
+        /// Offset to StringPool.Header defining the resource type symbol table.
+        type_strings: u32,
+        last_public_type: u32,
+        key_strings: u32,
+        last_public_key: u32,
+        type_id_offset: u32,
+    };
+
+    const Config = struct {
+        size: u32,
+        geo: union {
+            imsi: u32,
+            code: packed struct(u32) {
+                mcc: u16,
+                mnc: u16,
+            },
+        },
+        locale: union {
+            code: packed struct(u32) {
+                language: [2]u8,
+                country: [2]u8,
+            },
+            locale: u32,
+        },
+        screen_type: union {
+            details: packed struct(u32) {
+                orientation: u8,
+                touchscreen: u8,
+                density: u16,
+            },
+            screen_type: u32,
+        },
+        input: packed struct(u32) {
+            keyboard: u8,
+            navigation: u8,
+            input_flags: u8,
+            input_pad: u8,
+        },
+        screen_size: packed struct(u32) {
+            width: u16,
+            height: u16,
+        },
+        version: packed struct(u32) {
+            sdk: u16,
+            minor: u16,
+        },
+        screen_config: packed struct(u32) {
+            layout: u8,
+            ui_mode: u8,
+            smallest_screen_width_dp: u16,
+        },
+        screen_size_dp: packed struct(u32) {
+            width: u16,
+            height: u16,
+        },
+        locale_script: [4]u8,
+        locale_version: [8]u8,
+        screen_config2: packed struct(u32) {
+            layout2: u8,
+            color_mode: u8,
+            _pad: u16,
+        },
+        locale_script_was_computed: bool,
+        locale_numbering_system: [8]u8,
+    };
+
+    const TypeSpec = struct {
+        header: ResourceChunk,
+        id: u8,
+        res0: u8,
+        res1: u16,
+        entry_count: u32,
+    };
+
+    const Type = struct {
+        header: ResourceChunk,
+        id: u8,
+        flags: u8,
+        reserved: u8,
+        entry_count: u32,
+        entries_start: u32,
+        config: Config,
+    };
+
+    const SparseTypeEntry = struct {
+        entry: u32,
+        idx: u16,
+        offset: u16,
+    };
+
+    const Entry = struct {
+        size: u16,
+        flags: u16,
+        key: StringPool.Ref,
+    };
+
+    const MapEntry = struct {
+        parent: TableRef,
+        count: u32,
+    };
+
+    const Map = struct {
+        name: TableRef,
+        value: Value,
+    };
+
+    const LibHeader = struct {
+        header: ResourceChunk,
+        count: u32,
+    };
+
+    const LibEntry = struct {
+        package_id: u32,
+        package_name: [127:0]u16,
+    };
 };
 
 pub const Document = struct {
@@ -104,7 +317,7 @@ pub fn readAlloc(file: std.fs.File, alloc: std.mem.Allocator) !Document {
     while (i < string_table_count) : (i += 1) {
         // std.log.debug("{}", .{try file.getPos()});
         const len = try reader.readInt(u16, .Little);
-        const buf = string_buffer[pos..pos + 1 + len];
+        const buf = string_buffer[pos .. pos + 1 + len];
         pos += len + 1;
         for (buf) |*char| {
             char.* = try reader.readInt(u16, .Little);
@@ -115,7 +328,7 @@ pub fn readAlloc(file: std.fs.File, alloc: std.mem.Allocator) !Document {
     while (try reader.readInt(u32, .Little) != 0x00100102) {}
     std.log.debug("found start tag at {}", .{try file.getPos()});
 
-    var tag : [5]u32 = undefined;
+    var tag: [5]u32 = undefined;
     for (tag) |*value| {
         value.* = try reader.readInt(u32, .Little);
         std.log.debug("{}", .{value.*});
