3f6aad477ee77ead1a30e001df3daa42f946fc20
Louis Pearson <opensource@louispearson.work>
2023-04-30 18:11:18 -0600

    Update dependencies

--- build.zig
+++ build.zig
@@ -56,7 +56,7 @@ pub fn build(b: *std.Build) !void {
 
     b.installArtifact(exe);
 
-    const run_cmd = exe.run();
+    const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
     if (b.args) |args| {
         run_cmd.addArgs(args);
